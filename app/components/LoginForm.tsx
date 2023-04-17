d9c0528f84e990606f5c425f04da80799c856636
Itzac Albertin <itzacalbertin@gmail.com>
2023-04-17 17:59:42 -0300

    First commit, added login first elements

---
+++ app/components/LoginForm.tsx
@@ -0,0 +1,65 @@
+export default function LoginForm() {
+    return (
+        <div className="w-96">
+              <div className="bg-red-500 rounded-3xl px-9 py-12">
+
+                <form>
+                  <p className="mb-4 text-center text-2xl text-white font-bold">Login</p>
+  
+                  <div className="relative mb-3" data-te-input-wrapper-init>
+                    <input
+                      type="text"
+                      className="peer block min-h-[auto] w-full rounded-full border-0 bg-white px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
+
+                      placeholder="Username" />
+                    <label
+
+                      className="pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-primary"
+                      >Username
+                    </label>
+                  </div>
+
+    
+                  <div className="relative mb-6" data-te-input-wrapper-init>
+                    <input
+                      type="password"
+                      className="peer block min-h-[auto] w-full rounded-full border-0 bg-white px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
+                      placeholder="Password" />
+                    <label
+  
+                      className="pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-primary"
+                      >Password
+                    </label>
+                  </div>
+
+
+                  <div className="pt-1 grid place-items-center text-center">
+                    <button
+                      className="mb-3 inline-block w-32 rounded-full bg-white px-6 pb-2 pt-2.5 text-xs font-semibold uppercase leading-normal text-black shadow-[0_4px_9px_-4px_rgba(0,0,0,0.2)] transition duration-150 ease-in-out hover:shadow-[0_8px_9px_-4px_rgba(0,0,0,0.1),0_4px_18px_0_rgba(0,0,0,0.2)] focus:shadow-[0_8px_9px_-4px_rgba(0,0,0,0.1),0_4px_18px_0_rgba(0,0,0,0.2)] focus:outline-none focus:ring-0 active:shadow-[0_8px_9px_-4px_rgba(0,0,0,0.1),0_4px_18px_0_rgba(0,0,0,0.2)]"
+                      type="button"
+                      data-te-ripple-init
+                      data-te-ripple-color="light"
+                      >
+                      Log in
+                    </button>
+
+    
+                    <a className="text-white hover:text-gray-200 duration-200 transition" href="#!">Forgot password?</a>
+                  </div>
+
+    
+                  {/* <div className="flex items-center justify-between pb-6">
+                    <p className="mb-0 mr-2">Don't have an account?</p>
+                    <button
+                      type="button"
+                      className="inline-block rounded border-2 border-danger px-6 pb-[6px] pt-2 text-xs font-medium uppercase leading-normal text-danger transition duration-150 ease-in-out hover:border-danger-600 hover:bg-neutral-500 hover:bg-opacity-10 hover:text-danger-600 focus:border-danger-600 focus:text-danger-600 focus:outline-none focus:ring-0 active:border-danger-700 active:text-danger-700 dark:hover:bg-neutral-100 dark:hover:bg-opacity-10"
+                      data-te-ripple-init
+                      data-te-ripple-color="light">
+                      Register
+                    </button>
+                  </div> */}
+                </form>
+              </div>
+            </div>
+    )
+}
---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import LoginForm from './components/LoginForm'
+
+export default function Home() {
+  return (
+    <main className="grid place-items-center min-h-screen  p-24">
+      <LoginForm/>
+    </main>
+  )
+}
